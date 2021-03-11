197c6528fe2edb05a033b6b1f08f7925e3ff9f0e
Matthew <18314318+xavierblue@users.noreply.github.com>
2021-03-11 14:47:54 -0500

    Added illustrations
    Added illustrations to home and contact pages
    adjusted the page alignment for the contact page to match the home page
    simplified the code for controlling the page structure for the home page

--- MatthewSehorn/lib/views/home/home_view_full.dart
+++ MatthewSehorn/lib/views/home/home_view_full.dart
@@ -12,69 +12,63 @@ class HomeViewFull extends StatelessWidget {
     return Column(
       children: [
         Container(
-          margin: EdgeInsets.symmetric(
-            horizontal: 40,
-            vertical: 54,
+          margin: EdgeInsets.only(
+            left: 60,
+            right: 40,
+            top: 54,
+            bottom: 54,
           ),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Expanded(
-                flex: 2,
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flex(
-                        direction: Axis.horizontal,
-                        children: [
-                          Expanded(
-                            child: FittedBox(
-                              fit: BoxFit.fitWidth,
-                              child: SelectableText(
-                                Strings.homeDesciption,
-                                textAlign: TextAlign.left,
-                                style: Theme.of(context).textTheme.headline1.copyWith(
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Flex(
-                        direction: Axis.horizontal,
-                        children: [
-                          Expanded(
+              Flexible(
+                flex: 1,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flex(
+                      direction: Axis.horizontal,
+                      children: [
+                        Expanded(
+                          child: FittedBox(
+                            fit: BoxFit.fitWidth,
                             child: SelectableText(
-                              Strings.homeIntro,
+                              Strings.homeDesciption,
                               textAlign: TextAlign.left,
-                              style: Theme.of(context).textTheme.bodyText2.copyWith(
-                                    fontWeight: FontWeight.w400,
+                              style: Theme.of(context).textTheme.headline1.copyWith(
+                                    fontWeight: FontWeight.w600,
                                   ),
                             ),
                           ),
-                        ],
-                      ),
-                      // SizedBox(
-                      //   height: 28,
-                      // ),
-                      // Align(
-                      //   alignment: Alignment.centerLeft,
-                      //   child: DownloadCVButton(),
-                      // )
-                    ],
-                  ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Flex(
+                      direction: Axis.horizontal,
+                      children: [
+                        Expanded(
+                          child: SelectableText(
+                            Strings.homeIntro,
+                            textAlign: TextAlign.left,
+                            style: Theme.of(context).textTheme.bodyText2.copyWith(
+                                  fontWeight: FontWeight.w400,
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              Expanded(
-                flex: 3,
+              Flexible(
+                flex: 1,
                 child: Container(
                   margin: EdgeInsets.only(
-                    left: 36,
+                    left: 60,
                   ),
                   child: HomeAccentImage(),
                 ),
--- MatthewSehorn/lib/views/home/widgets/home_accent_image.dart
+++ MatthewSehorn/lib/views/home/widgets/home_accent_image.dart
@@ -6,41 +6,15 @@ class HomeAccentImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        margin: EdgeInsets.only(
-          bottom: 48,
+      constraints: BoxConstraints(
+        // maxHeight: 480,
+        maxWidth: 480,
+      ),
+      child: Image(
+        image: AssetImage(
+          'assets/images/home_accent_image.png',
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            12,
-          ),
-        ),
-        elevation: 16,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(
-              12,
-            ),
-          ),
-          margin: EdgeInsets.all(
-            16,
-          ),
-          child: Container(
-            constraints: BoxConstraints(
-              maxHeight: 320,
-              maxWidth: 800,
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(4),
-              child: Image(
-                image: AssetImage(
-                  'assets/images/home_accent_image.jpg',
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ),
+        fit: BoxFit.fitHeight,
       ),
     );
   }
