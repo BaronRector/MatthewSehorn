1f85410b4f9abbe928187f4b81e6bd8b04eaf195
Matthew <18314318+xavierblue@users.noreply.github.com>
2021-03-11 19:42:37 -0500

    Added theme cookie
    Added theme value to local storage.

--- MatthewSehorn/lib/services/theme_switch_service.dart
+++ MatthewSehorn/lib/services/theme_switch_service.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'dart:html' as html;
 
 class AppThemeState extends ChangeNotifier {
-  var isDarkModeEnabled = true;
+  var isDarkModeEnabled = (html.window.localStorage['appThemeMode'] == 'dark' || html.window.localStorage['appThemeMode'] == null);
 
   void setLightTheme() {
     isDarkModeEnabled = false;
+    html.window.localStorage['appThemeMode'] = 'light';
     notifyListeners();
   }
 
   void setDarkTheme() {
     isDarkModeEnabled = true;
+    html.window.localStorage['appThemeMode'] = 'dark';
     notifyListeners();
   }
 }
